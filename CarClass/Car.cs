e80518e770432cae2c6ea8b612579e2775067779
A.J. Miller <ajmiller1206@gmail.com>
2022-09-22 12:26:11 -0400

    first commit

---
+++ CarClass/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    public class Car
+    {
+        public string Make { get; set; } = "unknown";
+        public string Model { get; set; } = "unknown";
+        public int Year { get; set; }
+    }
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,11 @@
+﻿using CarClass;
+using Microsoft.VisualBasic;
+
+Car firstCar = new Car()
+{
+    Make = "Toyota",
+    Model = "Camry",
+    Year = 1998
+};
+
+Console.WriteLine($"My first car was a {firstCar.Year} {firstCar.Make} {firstCar.Model}.");
